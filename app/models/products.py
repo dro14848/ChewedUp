82e62adf8d145841f418983b723c054dad5a657a
Leandro <leandro.figueiredo14848@gmail.com>
2023-03-21 17:44:46 -0400

    added collums on products

--- app/models/products.py
+++ app/models/products.py
@@ -9,5 +9,21 @@ class Product(db.Model):
         __table_args__ = {'schema': SCHEMA}
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50), nullable=False)
+    name = db.Column(db.String(50))
+    description = db.Column(db.String(255))
+    price = db.Colum(db.Float())
+    type = db.Colum(db.String(50))
+
+
     
+    reviews = db.relationship("Review", back_populates="product", cascade='all, delete')
+    productimages = db.relationship("ProductImages", back_populates="product", cascade='all, delete')
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'description': self.description,
+            'price': self.price,
+            'type': self.type
+        }
