931461a03bc0e56db175681863a0192ac80e48dc
Leandro <leandro.figueiredo14848@gmail.com>
2023-03-21 17:27:11 -0400

    reviews

--- app/models/pet_profile.py
+++ app/models/pet_profile.py
@@ -2,12 +2,10 @@ from .db import db, environment, SCHEMA, add_prefix_for_prod
 from sqlalchemy.orm import relationship
 
 
-class Product(db.Model):
-    __tablename__="product"
+class PetProfile(db.Model):
+    __tablename__="petprofile"
 
     if environment == "production":
         __table_args__ = {'schema': SCHEMA}
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50), nullable=False)
-    
