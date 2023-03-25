0f4d2e4fbee7e27e7d282249f3395c21693c841e
Leandro <leandro.figueiredo14848@gmail.com>
2023-03-25 15:44:05 -0400

    single product route

--- app/api/product_routes.py
+++ app/api/product_routes.py
@@ -1,10 +1,10 @@
 from flask import Blueprint, jsonify, request
-from app.models import db, Product
+from app.models import db, Product, ProductImages
 
 
 product_routes = Blueprint('products', __name__)
 
-
+# GET ALL PRODUCTS
 @product_routes.route('/')
 def allProducts():
     products = Product.query.all()
@@ -15,3 +15,13 @@ def allProducts():
         pd.update(pdImages)
         allProducts.append(pd)
     return {'products': allProducts}
+
+# GET ONE PRODUCT
+@product_routes.route('<int:id>')
+def singleProduct(id):
+    product = Product.query.get(id)
+    images = ProductImages.query.get(id)
+    image_dict = images.to_dict()
+    product_dict = product.to_dict()
+    product_dict.update(image_dict)
+    return product_dict
