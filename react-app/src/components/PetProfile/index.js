9e025eee6599d6ef4a13babde2decb25d2a87403
Leandro <leandro.figueiredo14848@gmail.com>
2023-04-25 16:40:33 -0400

    R&D

---
+++ react-app/src/components/PetProfile/index.js
@@ -0,0 +1,10 @@
+import {useEffect} from 'react'
+import {useDispatch, useSelector} from 'react-redux'
+import { NavLink, Switch, Route } from 'react-router-dom'
+
+
+function PetProfile () {
+
+}
+
+export default PetProfile
